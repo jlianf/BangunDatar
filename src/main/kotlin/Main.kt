266c32e72d97369fd5629224b3c3e7086131d382
jalian Febriandy <jalianfebriandy15@gmail.com>
2022-12-31 12:50:34 +0700

    add class Segitiga

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,26 @@
 fun main(args: Array<String>) {
     println("Menghitung Bangun Datar")
     val persegi = Persegi()
+    val persegiPanjang = PersegiPanjang()
+    val segitiga = Segitiga()
+
     println("=======================")
     println("MENGHITUNG PERSEGI")
-    println(persegi.luas())
-    println(persegi.keliling())
+    persegi.luas()
+    persegi.keliling()
+    println("=======================")
 
-    println("========================")
+    println("=======================")
     println("MENGHITUNG PERSEGI PANJANG")
+    persegiPanjang.luas()
+    persegiPanjang.keliling()
+    println("=======================")
+
+    println("=======================")
+    println("MENGHITUNG Segitiga")
+    segitiga.luas()
+    segitiga.keliling()
+    println("=======================")
 
 }
 
@@ -39,7 +52,20 @@ class PersegiPanjang() : BangunDatar(){
     }
 
     override fun keliling() {
-        super.keliling()
+        println("Menghitung Keliling Persegi Panjang = ${2*(panjang*lebar)}")
     }
 
+}
+class Segitiga() : BangunDatar() {
+    val tinggi = 4
+    val alas = 5
+    val sisi3 = 2
+
+    override fun luas() {
+        println("Menghitung Luas Segitiga = ${2/(alas*tinggi)}")
+    }
+
+    override fun keliling() {
+        println("Menghitung Keliling Setigita = ${tinggi+alas+sisi3}")
+    }
 }
